ab41510217aa59b12a0259328230492066c1db5f
Mohit <mohit.singh@highlande.rs>
2020-05-30 11:05:51 +0530

    remove all console

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-console.log('MakeloGy')
-console.log('MakeloGy1')
-console.log('MakeloGy2')
-console.log('MakeloGy4')
-console.log('MakeloGy8')
-console.log('MakeloGy16')
-console.log('MakeloGy32')
-
-
 
