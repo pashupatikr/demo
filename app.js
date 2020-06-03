f3c51ba82475d1c3347786fa76215051826c9339
Mohit <mohit.singh@highlande.rs>
2020-06-03 21:33:29 +0530

    add triangle code

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+function makeLine(x,length) {
+    var line = "";
+    var sp = "";
+    for (var j = 1; j <= x; j++) {
+        line += "* ";
+    }
+    for (var y = 1; y <= length - x; y++) {
+        sp += " ";
+    }
+    return sp + line + "\n"; 
 
-console.log('MakeloGy')
-console.log('MakeloGy1')
-console.log('MakeloGy100')
-console.log('MakeloGy4')
-console.log('MakeloGy5')
+} 
 
-
-
-
+function buildTriangle(length) {
+    var triangle = ""; 
+    for (var x = 1; x <= length; x++) {
+        triangle += makeLine(x,length);
+    }
+    return triangle;
+} 
+console.log(buildTriangle(10));
